aaa606f5d23758054cef59f22bd39d6ce160a418
guillermo.gomez <guillermo.gomezdr@gmail.com>
2016-11-18 14:30:41 +0100

    ejemplos varios de c

---
+++ 01_cambio_de_base/base.cpp
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+	int numero;
+
+	printf("Introduce un número: ");
+	scanf(" %i", &numero); 
+	//Aquí recibiremos la información del teclado. 
+	//Dejaremos un espacio para eliminar todos los espacios que nos introduzca el usuario.
+	//el %i le indicamos a la tubería que lea un entero. 
+	//el &numero averigua la dirección de la variable numero, y lo coloca allí.
+	
+	//Hacer 
+	//	Imprimiremos numero % 2
+	//	Numero = numero /2
+	//Mientras numero /2 > 0
+	
+       	do {
+		printf("%i", numero % 2);
+		numero /= 2;
+	} while (numero>0);	
+
+	printf("\n");
+
+	return EXIT_SUCCESS;
+}
