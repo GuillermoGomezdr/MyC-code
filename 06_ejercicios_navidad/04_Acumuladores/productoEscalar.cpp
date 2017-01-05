473eb308ed8aeca29338a230adceb22370853fec
“GuillermoGomezdr” <guillermo.gomezdr@gmail.com>
2017-01-05 13:23:06 +0100

    Ejercicios Navidad. Rehaciendo ejercicios que no me salían de Acumuladores. Este apartado, ya terminado.

--- 06_ejercicios_navidad/04_Acumuladores/productoEscalar.cpp
+++ 06_ejercicios_navidad/04_Acumuladores/productoEscalar.cpp
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 
 int main () {
-	//En proceso.	
+	
+	int vector1[2], vector2[2];
+	int productoE;
+
+	printf("Introduzca el valor X del primer vector\n");
+	scanf(" %i", &vector1[0]);	
+	printf("Introduzca el valor Y del primer vector\n");
+	scanf(" %i", &vector1[1]);	
+	
+	printf("Introduzca el valor X del segundo vector\n");
+	scanf(" %i", &vector2[0]);	
+	printf("Introduzca el valor Y del segundo vector\n");
+	scanf(" %i", &vector2[1]);
+
+	productoE = vector1[0] * vector2[0] + vector1[1] * vector2[1];
+	printf("El producto escalar de ambos vectores será: %i.\n", productoE);
+	
+
 	return EXIT_SUCCESS;
 }
