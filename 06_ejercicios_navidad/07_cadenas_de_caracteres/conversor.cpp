d547f02e94708675a5c79d0e5f894d117bda4707
“GuillermoGomezdr” <guillermo.gomezdr@gmail.com>
2017-01-10 10:29:22 +0100

    Ejercicios navidad. Rehaciendo ejercicios que no salían

--- 06_ejercicios_navidad/07_cadenas_de_caracteres/conversor.cpp
+++ 06_ejercicios_navidad/07_cadenas_de_caracteres/conversor.cpp
@@ -6,13 +6,16 @@ int main () {
 	//No entiendo muy bien el enunciado, pero voy a probar a ver.
 	//Nota 2: No me funciona, lo dejo para luego.
 
-	char dni[8] = "0000000";
-	scanf(" %c", dni);
-	int convertido;
+	char dni[9] = "00000000";
+	printf("Introduce el número de tu dni\n");
+	scanf(" %s", dni);
+	int convertido[9] = {0,0,0,0,0,0,0,0};
+
 	
-	
-	convertido = atoi(dni);
-	printf(" %i", convertido);	
-	
+	//convertido = atoi(dni);
+	for(int i = 0; i < sizeof(dni)/sizeof(char)-1; i++){
+		printf(" %i", dni[i]);	
+	}
+	printf(" \n");
 	return EXIT_SUCCESS;
 }
--- 06_ejercicios_navidad/10_primitivas_entrada_salida/nombreApellido2.cpp
+++ 06_ejercicios_navidad/10_primitivas_entrada_salida/nombreApellido2.cpp
@@ -7,9 +7,11 @@ int main () {
 	char nombre[15], apellido[15];
 
 	printf("Introduce tu nombre:\n");
-	scanf(" %s", nombre);
+	//scanf(" %s", nombre);
+	fgets(nombre, 15, stdin);
 	printf("Introduce tu apellido:\n");
-	scanf(" %s", apellido);
+	//scanf(" %s", apellido);
+	fgets(apellido, 15, stdin);
 
 	sprintf(nombreApellido, "%s %s", nombre, apellido);
 	printf("Te llamas: %s.\n", nombreApellido);
