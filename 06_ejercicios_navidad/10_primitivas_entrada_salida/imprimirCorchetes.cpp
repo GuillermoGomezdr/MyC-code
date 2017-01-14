98d84d435eda7ce6b9e57e1845ca27ca189e659c
“GuillermoGomezdr” <guillermo.gomezdr@gmail.com>
2017-01-14 15:06:32 +0100

    Ejercicios navidad. Rehaciendo ejercicios que no me salían.

--- 06_ejercicios_navidad/10_primitivas_entrada_salida/imprimirCorchetes.cpp
+++ 06_ejercicios_navidad/10_primitivas_entrada_salida/imprimirCorchetes.cpp
@@ -11,13 +11,15 @@ int main () {
 	letra = fgetc(fichero);
 
 	while(letra >= 0){
-		//printf("imprimiendo es: %i\n", imprimiendo);
-		if(letra == 91)
+		if(letra == 91){
 			imprimiendo = 1;
-		else if(letra == 93)
+		}else if(letra == 93){
+			printf("    ");
 			imprimiendo = 0;
-		if(imprimiendo = 1)
+		}
+		if(imprimiendo == 1 && letra != 91){
 			printf(" %c", letra);
+		}
 		letra = fgetc(fichero);
 	}
 	printf("\n");
--- 06_ejercicios_navidad/12_estructuras_iterativas/escribirCuadrado.cpp
+++ 06_ejercicios_navidad/12_estructuras_iterativas/escribirCuadrado.cpp
@@ -2,8 +2,21 @@
 #include <stdlib.h>
 
 int main () {
+
+	int num;
+
+	printf("Introduce el tamaño del que quieres que sea el cuadrado un asteriscos:\n");
+	scanf(" %i", &num);
+
+	for(int i = 0; i < num; i++){
+		for(int o = 0; o < num; o++){
+			printf("* ");
+		}
+		printf(" \n");
+	}
 	
-	printf("* * * * *\n* * * * *\n* * * * *\n* * * * *\n* * * * *\n");
+	printf("\n");
+	//printf("* * * * *\n* * * * *\n* * * * *\n* * * * *\n* * * * *\n");
 
 	return EXIT_SUCCESS;
 }
