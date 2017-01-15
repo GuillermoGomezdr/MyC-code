54574b6604eb636b2eb9ecf62d119fc2f4486c3d
“GuillermoGomezdr” <guillermo.gomezdr@gmail.com>
2017-01-15 12:14:26 +0100

    Ejercicios navidad. Rehaciendo ejercicios que no me salían.

--- 06_ejercicios_navidad/07_cadenas_de_caracteres/conversor.cpp
+++ 06_ejercicios_navidad/07_cadenas_de_caracteres/conversor.cpp
@@ -4,18 +4,17 @@
 int main () {
 
 	//No entiendo muy bien el enunciado, pero voy a probar a ver.
-	//Nota 2: No me funciona, lo dejo para luego.
 
 	char dni[9] = "00000000";
 	printf("Introduce el número de tu dni\n");
 	scanf(" %s", dni);
-	int convertido[9] = {0,0,0,0,0,0,0,0};
+	int convertido;
 
-	
-	//convertido = atoi(dni);
-	for(int i = 0; i < sizeof(dni)/sizeof(char)-1; i++){
+	convertido = atoi(dni);
+
+	printf(" El resultado final es: %i\n", convertido);
+	/*for(int i = 0; i < sizeof(dni)/sizeof(char)-1; i++){
 		printf(" %i", dni[i]);	
-	}
-	printf(" \n");
+	}*/
 	return EXIT_SUCCESS;
 }
