14f5adb8f9cb9c062e4bf6df70aa2d11517883f8
“GuillermoGomezdr” <guillermo.gomezdr@gmail.com>
2017-01-19 10:10:20 +0100

    Trabajando en el ahorcado

--- 07_strings_dyn/ahorcado.cpp
+++ 07_strings_dyn/ahorcado.cpp
@@ -1,43 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #define N 0x200
 #define CANTIDAD(x) (sizeof((x))/sizeof(char *)-1)
 
-const char *palabra[] = {"patata", "amalgama", "narcolepsia", "suricato", 
-"parguela", "shuriken", "panenteismo", "nemesis", "antitesis", "origen",
-"artropodo", NULL};
+	const char *palabra[] = {"patata", "amalgama", "narcolepsia",
+	"suricato", "parguela", "shuriken", "panenteismo", "nemesis", 
+	"antitesis", "origen","artropodo", NULL};
 
 int main () {
-	char buffer[N];
-	char *palabra;
+
+	char adivinado[N];
+	const char *elegida;
 	char letra;
-	int longitudPalabra;
+	int aleatorio, fin = 0;
 
-	printf("Intrduzca una palabra:");
-	fgets(buffer, N, stdin);
+	//Primera parte.
+	//Tomamos una palabra al azar del char *palabra[]
+	srand(time(NULL));
+	aleatorio = rand() % CANTIDAD(palabra);		
+	elegida = palabra[aleatorio];
 
-	longitudPalabra = strlen(buffer)+1;
-	palabra = (char *) malloc(strlen(buffer)+1);
+	printf(" %s\n", elegida);
 	
-	/*for(int i=0; i<strlen(elegida); i++)
-	 	adivinado[i] = '_';
+	//Rellenamos de ceros el adivinado y luego de '_'. Su cantidad igualará 
+	//al nº de caracteres de la palabra seleccionada.
+	bzero(adivinado, N);
+	for(int i=0; i<strlen(elegida); i++)
+		adivinado[i]='_';
 
-	*/
+	//printf(" %s\n", adivinado);
+	
 
-	printf("Intenta adivinar la palabra.\n Introduce una letra:");
+	//Segunda parte.
+	//Pediremos letras.
+	printf("Introduzca una letra");
 	scanf(" %c", &letra);
-	for(int i = 0; i < longitudPalabra; i++){
-		if(letra == buffer[i]){
-		printf("patata");	
+	
+	for(int i=0; i<strlen(elegida); i++){
+		if(letra == elegida[i]){
+			adivinado[i] = letra;
 		}
 	}
 
-	
-	//strcpy(palabra, buffer);
-	//printf("La palabra que has escrito es: %s\n", palabra);
-
-	free(palabra);
+	printf(" %s\n", adivinado);
 	return EXIT_SUCCESS;
 }
