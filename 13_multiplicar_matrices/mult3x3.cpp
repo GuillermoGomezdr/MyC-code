2ba2e405c471eaf6b1e4f9d72dc61a84476aa195
GuillermoGomezdr <guillermo.gomezdr@gmail.com>
2017-03-08 13:45:52 +0100

    Intentando hacer cosas

---
+++ 13_multiplicar_matrices/mult3x3.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main () {
+
+	int dim1_m1 = 3;
+	int dim1_m2 = 3;
+
+	int dim2_m1 = 3;
+	int dim2_m2 = 3;
+
+	//int matriz1[dim1_m1][dim1_m2] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	//int matriz2[dim2_m1][dim2_m2] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+
+	/*
+	if(dim1_m2 == dim2_m1){
+		int matrizResultado[dim1_m1][dim2_m2];
+		for(int i = 1; i <= dim_m1; i++){
+			for(int o = 1; i <= sim2_m2; o++){
+				matrizResultado[i][o] = 
+			}
+		}
+	}
+	*/	
+
+	return EXIT_SUCCESS;
+}
