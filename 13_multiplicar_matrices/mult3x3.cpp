871e7c01e2f2b8c8ca70f10479d949c89b267b78
GuillermoGomezdr <guillermo.gomezdr@gmail.com>
2017-03-09 11:30:48 +0100

    cosa rara que no sale. Revisar

--- 13_multiplicar_matrices/mult3x3.cpp
+++ 13_multiplicar_matrices/mult3x3.cpp
@@ -2,27 +2,32 @@
 #include <stdlib.h>
 
 
+#define dim1_m1 3
+#define dim1_m2 3
+#define dim2_m1 3
+#define dim2_m2 3
+
 int main () {
+	int matriz1[dim1_m1][dim1_m2] = {{1, 2, 3}, {2, -3, 1}, {-4, 3, 2}};
+	int matriz2[dim2_m1][dim2_m2] = {{7, 2, 5}, {1, 2, 4}, {-3, 2, 1}};
+		
+	int matrizResultado[dim1_m1][dim2_m2] ={{0, 0, 0}, {0, 0, 0}, {0, 0, 0}} ;
+	
 
-	int dim1_m1 = 3;
-	int dim1_m2 = 3;
-
-	int dim2_m1 = 3;
-	int dim2_m2 = 3;
-
-	//int matriz1[dim1_m1][dim1_m2] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-	//int matriz2[dim2_m1][dim2_m2] = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
-
-	/*
+	
+	
 	if(dim1_m2 == dim2_m1){
-		int matrizResultado[dim1_m1][dim2_m2];
-		for(int i = 1; i <= dim_m1; i++){
-			for(int o = 1; i <= sim2_m2; o++){
-				matrizResultado[i][o] = 
+		for(int i = 1; i <= dim1_m1; i++){
+			for(int o = 1; o <= dim2_m2; o++){
+				for(int k = 1; k <= dim2_m1; k++){
+					matrizResultado[i][o] += matriz1[i][k] * matriz2[k][o];  
+				}
+			printf("%i ", matrizResultado[i][o]);
 			}
+			printf("\n");
 		}
 	}
-	*/	
+		
 
 	return EXIT_SUCCESS;
 }
