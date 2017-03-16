d29a3ea36d5e55607d3c2659fa2a842cb16aed89
GuillermoGomezdr <guillermo.gomezdr@gmail.com>
2017-03-16 12:27:21 +0100

    4 en raya terminado (a lo cutre, pero terminado), falta revisar ciertos fallos.

--- 14_juego/4raya.cpp
+++ 14_juego/4raya.cpp
@@ -3,6 +3,47 @@
 
 #define N 5
 
+
+int comprobarGanador(char cg_ficha, char cg_tablero[N][N]){
+	for (int i = 0; i < N; i++){
+		for(int o = 0; o < N; o++){
+			if(cg_tablero[i][o] == cg_ficha){		
+				if(cg_tablero[i+1][o] == cg_ficha && 
+				   cg_tablero[i+2][o] == cg_ficha && 
+				   cg_tablero[i+3][o] == cg_ficha){
+				
+					printf("¡El jugador %c ha hecho 4 en raya!\n", cg_ficha);
+					return 0;
+
+				} else if(cg_tablero[i][o+1] == cg_ficha && 
+				   cg_tablero[i][o+2] == cg_ficha && 
+				   cg_tablero[i][o+3] == cg_ficha){
+				
+					printf("¡El jugador %c ha hecho 4 en raya!\n", cg_ficha);
+					return 0;
+
+				} else if(cg_tablero[i-1][o+1] == cg_ficha && 
+				   cg_tablero[i-2][o+2] == cg_ficha && 
+				   cg_tablero[i-3][o+3] == cg_ficha){
+				
+					printf("¡El jugador %c ha hecho 4 en raya!\n", cg_ficha);
+					return 0;
+				
+				} else if(cg_tablero[i+1][o+1] == cg_ficha && 
+				   cg_tablero[i+2][o+2] == cg_ficha && 
+				   cg_tablero[i+3][o+3] == cg_ficha){
+				
+					printf("D¡El jugador %c ha hecho 4 en raya!\n", cg_ficha);
+					return 0;
+					
+				}
+			}
+		}
+	}
+	return 1;	
+}
+
+
 int main () {
 
 	char tablero[N][N];
@@ -35,11 +76,15 @@ int main () {
 				}
 				printf("\n");
 			}
+		
+		game = comprobarGanador(ficha, tablero);
+	
 		if(ficha == 'x')
 			ficha = 'o';
 		else if(ficha == 'o')
 			ficha = 'x';	
 		}	
+
 	}
 
 	return EXIT_SUCCESS;
