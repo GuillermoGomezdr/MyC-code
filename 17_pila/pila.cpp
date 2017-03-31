623009a4b75eb42418db1fd89039256e2100ac1c
GuillermoGomezdr <guillermo.gomezdr@gmail.com>
2017-03-31 09:29:23 +0200

    Pila terminada y comentada. ;)

--- 17_pila/pila.cpp
+++ 17_pila/pila.cpp
@@ -44,14 +44,18 @@ struct TCoord *valor_random(){
 	//podremos y guardaremos el dato que corresponde al pasar el valor de la dirección de memoria de
 	//la estructura TCoord.
 bool push(struct TPila *pila, struct TCoord *dato){
-	if(pila -> cima >= Max_Pila)
+	if(pila -> cima >= Max_Pila){
+		printf("La pila ha alcanzado su tope. Vacía alguna posición antes de volver a intentar añadir más.\n");
 		return false;
+	}
 	pila -> data[pila->cima++] = dato;
+	printf("Añadimos a la pila la posición %i, que contiene: (%i, %i)\n", pila->cima-1,pila->data[pila->cima-1]->x, pila->data[pila->cima-1]->y);
+
 	return true;
 }
 
 //Crearemos otra FUNCION de tipo booleano.
-//A esta función oslo le pasaremos la dirección de memoria de la estructura TPila.
+//A esta función solo le pasaremos la dirección de memoria de la estructura TPila.
 	//En primer lugar, comprobaremos que el tope de la cima, no es ni cero, ni por algún casual, algún
 	//número menor. Si se da el caso, significará que no tendrémos nada en la pila, por lo que
 	//devolveremos false, y no haremos nada más.
@@ -71,16 +75,36 @@ bool push(struct TPila *pila, struct TCoord *dato){
 		//Por tanto, tomaremos el valor numérico de la cima en la 
 		//estructura TPila, y luego le restaremos uno.
 bool pop(struct TPila *pila){
-	if(pila -> cima <= 0)
+	if(pila -> cima <= 0){
+		printf("Pila vacía.\n");
 		return false;
+	}
+	printf("Quitamos de la pila la posición %i, que contiene: (%i, %i)\n", pila->cima-1,pila->data[pila->cima-1]->x, pila->data[pila->cima-1]->y);
 	pila -> cima--;
-	free(pila->data[--pila->cima]);
+	free(pila->data[pila->cima]);
 	return true;
 
 }
 
+//Crearemos una FUNCION de tipo void, que servirá para mostrarnos la pila.
+//Simplemente, le pasaremos la estructura TPila e iremos recorriendo los valores de pila.data[i]
+//(tanto x como y) hasta que hayamos recorrido la totalidad de sus posiciones.
+void mostrar(struct TPila pila){
+	for(int i = 0; i<pila.cima; i++)
+		printf("Posicion %i de la pila: (%i, %i)\n", i, pila.data[i]->x, pila.data[i]->y);
+}
  
-	//Main en proceso, sin terminar.
+
+//El main es lo más sencillo:
+//Definimos una estructura de tipo TPila...
+//... y la rellenamos de ceros.
+
+//Definimso también una variable tipo int, llamada num, que usaremos para que el usuario nos indique
+//que quiere hacer con la pila. Luego le mostraremos que opciones tiene para que pueda seleccionar la
+//que quiera.
+
+//Crearemos un bucle while que se recorrerá todo el rato, hasta que el usuario introduzca el número 5.
+//El resto de números llamarán a la función que le corresponda.
 int main () {
 
 	struct TPila objConCoord;
@@ -100,8 +124,8 @@ int main () {
 			while(pop(&objConCoord));
 			printf("Borrado.\nPila ahora vacía.\n");
 		}
-		else if(num == 4)					//Esto está en proceso. Continuar.
-			printf("En proceso\n");
+		else if(num == 4)
+			mostrar(objConCoord);
 		else if(num ==5)
 			return EXIT_SUCCESS;
 		else
