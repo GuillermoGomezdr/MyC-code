8889568eb071038f6d7eed5a9fa83c02e867258c
GuillermoGomezdr <guillermo.gomezdr@gmail.com>
2017-10-03 20:12:35 +0200

    Cada cosa hecha por separado.

--- 26_getopt_long/conversion.cpp
+++ 26_getopt_long/conversion.cpp
@@ -4,8 +4,10 @@
 #include <math.h>
 
 #include <getopt.h> //Librería que nos permitirá usar getopt_long
+
 #define DEBUG(...) if (verbose) \
 	fprintf(stderr, __VA_ARGS__);
+#define NUM atoi(argv[argc-1])
 
 const char* program_name;
 int verbose = 0;
@@ -23,14 +25,36 @@ int convertTo(int base, int num, int elevadoA){
 	return num%base*pow(10,elevadoA++) + convertTo(base, num/base, elevadoA);
 }
 
+int convertFrom(int base, int num, int elevadoA){
+	// PARTE 1:
+	//	Averiguar la longitud del num
+	if(elevadoA == -1)
+		while(num > pow(10, elevadoA))		
+			elevadoA++;
+	elevadoA --;
+
+	//PARTE 2:
+	//	
+	int numSacado = num / pow(10, elevadoA);
+
+	if(num < 10)
+		return numSacado * pow(base, elevadoA);	
+
+	num = num - (numSacado * pow(10, elevadoA));
+
+	return numSacado* pow(base, elevadoA) + convertFrom(base, num, elevadoA);		
+}
+
 int main (int argc, char* const argv[]) {
 	int next_option;
 	int output_base;
+	int input_base;
 	int numConvertido = 0;
 
-	const char* const short_options = "hb:v";
+	const char* const short_options = "hI:b:v";
 	const struct option long_options[] ={
 		{"help",	0,	NULL,	'h'},
+		{"ibase",	1,	NULL,	'I'},
 		{"base",	1,	NULL,	'b'},
 		{"verbose",	0,	NULL,	'v'}
 	};
@@ -43,13 +67,20 @@ int main (int argc, char* const argv[]) {
 			case 'h':
 				print_usage(stdout, EXIT_SUCCESS);
 				break;
+			case 'I':
+				input_base = atoi(optarg);		
+				printf("La I es %i\n", input_base);
+				numConvertido = convertFrom(input_base, atoi(argv[argc-1]),-1);
+				printf("El número %i, de base %i, pasado a base 10 es %i.\n", 
+					NUM, input_base, numConvertido);
+				break;
 			case 'b':
 				output_base = atoi(optarg);
 				DEBUG("Estableciendo la base de salida a %i\n", output_base);
 				
-				numConvertido = convertTo(output_base, atoi(argv[argc-1]), 0);	
+				numConvertido = convertTo(output_base, NUM, 0);	
 				printf("El número %i, pasa a base %i, y da %i.\n",
-					atoi(argv[argc-1]), output_base, numConvertido);
+					NUM, output_base, numConvertido);
 				
 				break;
 			case 'v':
