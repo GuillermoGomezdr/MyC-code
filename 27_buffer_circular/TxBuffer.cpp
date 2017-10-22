89dc2592d2e791297d4c80a10a94c2207efa34fe
GuillermoGomezdr <guillermo.gomezdr@gmail.com>
2017-10-22 16:17:25 +0200

    Subiendo buffer. Falta corregir y terminar.

---
+++ 27_buffer_circular/TxBuffer.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 5
+#define LONGITUD(x) ((x)-> summit - (x) -> head)
+
+struct TBuffer{
+	int data[N];
+	int head;
+	int summit;
+}
+
+void push(struct TBuffer *b, int d){
+	if(LONGITUD(b) >=N)
+		return;
+
+	b-> data[(b->summit)%N] = d;
+	b -> summit++;
+}
+
+int shift (struct TBuffer *b){ //Esto vendría siendo el pop
+	int d = b -> data[(b -> head)%N];
+	b -> head++;
+	return d;
+}
+
+int main () {
+	int d;
+	struct TBuffer buffer;
+	buffer.head = 0;
+	buffer.summit = 0;
+
+	push(&buffer,d);
+	d = shift(&buffer);
+
+	//peep(buffer);
+
+	return EXIT_SUCCESS;
+}
